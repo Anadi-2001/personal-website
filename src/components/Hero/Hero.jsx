842e07fb9f1e424cde9e26dd37205718b2955aea
Anadi <anadipatra44@gmail.com>
2025-03-31 22:16:45 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Anadi</h1>
+        <p className={styles.description}>
+        "I’m a fresher full-stack developer skilled in React and Node.js. Reach out if you’d like to learn more!"
+
+Let me know if you want a different variation!
+        </p>
+        <a href="Contact:9778885344" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
